d6d47cbac838cca8750f04cb59f473e4b5ce1698
zeineff <zeineff@gmail.com>
2018-11-15 19:41:39 +0000

    Add files via upload

--- Storog/functions/api_calls.php
+++ Storog/functions/api_calls.php
@@ -3,15 +3,25 @@
         $steam_search = "https://store.steampowered.com/api/appdetails?appids=" . $appid;
 
         $json = json_decode( file_get_contents($steam_search), true );
-
-	$price = $json[$appid]["data"]["price_overview"]["initial"];
-
+        $data = $json[$appid]["data"];
+        
         $game_data = array(
-            "title" => $json[$appid]["data"]["name"],
-            "description" => $json[$appid]["data"]["short_description"],
-            "price" => $price,
-            "image" => $json[$appid]["data"]["header_image"]
+            "title" => $data["name"],
+            "description" => $data["short_description"],
+            "image" => $data["screenshots"]["0"]["path_thumbnail"],
+            "header_image" => $data["header_image"],
+            "is_free" => $data["is_free"],
+            "released" => !$data["release_date"]["coming_soon"],
+            "developers" => $data["developers"]
 	);
+        
+        if (!$data["is_free"]){
+            if (!$data["release_date"]["coming_soon"]){
+                $game_data["price"] = $data["price_overview"]["final"];
+            }
+        }
+        
+        $game_data["release_date"] = $data["release_date"]["date"];
 
 	return $game_data;
     }
