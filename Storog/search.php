e7eb204a4e64a8bc331fddc3a539281288a62e6c
zeineff <zeineff@gmail.com>
2018-11-16 13:38:37 +0000

    Add files via upload

--- Storog/search.php
+++ Storog/search.php
@@ -1,7 +1,7 @@
 <?php
     include_once("functions/functions.php");
     
-    $query = filter_input(INPUT_GET, "game_title", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $query = filter_input(INPUT_GET, "query", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $games = get_games_by_title($query);
     $games_found = sizeof($games) !== 0;
 ?>
@@ -24,7 +24,7 @@
                 echo "<p>No games found.</p>";
             }else{
                 foreach ($games as $game) : ?>
-                    <a href="game.php?game_id=<?php echo $game["steam_id"] ?>">
+                    <a href="game.php?game_id=<?php echo $game["id"] ?>">
                         <?php echo $game["title"] ?>
                     </a><br>
                 <?php endforeach;
