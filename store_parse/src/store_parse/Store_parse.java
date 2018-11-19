ae0d5386d009eecd2f7b7e078bdb3f82d59a37cb
zeineff <zeineff@gmail.com>
2018-11-19 14:57:01 +0000

    Add files via upload

--- store_parse/src/store_parse/Store_parse.java
+++ store_parse/src/store_parse/Store_parse.java
@@ -1,8 +1,11 @@
 package store_parse;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -60,6 +63,7 @@ public class Store_parse{
             for (int i = 0; i < game_list.length(); i++){
                 JSONObject game = game_list.getJSONObject(i);
                 String title = game.getString("title");
+                title = remove_legal_chars(title);
                 
                 Game g = games.get(title);
                 
@@ -100,17 +104,54 @@ public class Store_parse{
         return null;
     }
     
-    public static void output(){
-        int x = 0;
-        
-        for (Map.Entry pair : games.entrySet()){
-            String title = (String) pair.getKey();
-            Game game = (Game) pair.getValue();
-            System.out.println(game);
+    public static String remove_legal_chars(String title){
+        for (int i = 0; i < title.length(); i++){
+            char c = title.charAt(i);
             
-            x++;
-            if (x >= 10)
-                break;
+            if (c == '™' || c == '©')
+                title = title.substring(0, i) + title.substring(i + 1, title.length());
         }
+        
+        return title;
+    }
+    
+    public static void output(){
+        String format = "INSERT INTO GAMES (title, steam_id, on_gog) VALUES ('%s', %s, %s)";
+        
+        try {
+            PrintWriter out = new PrintWriter("sql.txt", "UTF-8");
+        
+            for (Map.Entry pair : games.entrySet()){
+                String title = (String) pair.getKey();
+                Game game = (Game) pair.getValue();
+                
+                title = encode_special_chars(title);
+                String steam = (game.steam == null ? "NULL" : "" + game.steam.steam_id);
+                String gog = (game.gog == null ? "0" : "1");
+
+                String query = String.format(format, title, steam, gog);
+                out.println(query);
+            }
+            
+            out.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Error writing output file");;
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Error encoding certain characters");;
+        }
+    }
+    
+    public static String encode_special_chars(String s){
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if (c == '\'' || c == '"' || c == '\\'){
+                s = s.substring(0, i) + '\\' + s.substring(i);
+                i++;
+            }
+        }
+        
+        return s;
     }
 }
